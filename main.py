4dc3218ea2bfebc9604036d9433c9f43ff484cbc
tommcn <tommcn@mcnamer.ca>
2020-10-26 07:44:23 -0400

    p3

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import sys
+import os
 from webserver import *
 from ai import *
 import threading
@@ -6,6 +7,6 @@ import sys
 
 print("Starting AI Engine Server...")
 if (len(sys.argv) > 1):
-    webApi.run(host='0.0.0.0', port="80")
+    webApi.run(host='0.0.0.0', port=os.environ.get("PORT"))
 else:
     webApi.run(host='0.0.0.0', port="5500")
